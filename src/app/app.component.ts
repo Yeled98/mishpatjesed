77d43cc2bb0235ef0566309039f61c2b0d6bb4be
Yeled Subim Gonzalez Montalban <yeledsubim@gmail.com>
2022-07-18 19:34:27 -0500

    Adding Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'mishpatjesed';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    var myCarousel = document.querySelector('#myCarousel')
+    var carousel = new bootstrap.Carousel(myCarousel)
+  }
 }
